14aecd54159829292640d10190fa54a989754a35
sanjee <146153321+sanjee23@users.noreply.github.com>
2025-06-13 05:37:42 +0800

    main

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,23 +1,17 @@
-<script setup>
-import HeaderNavbar from 'components/HeaderNavbar.vue'
-</script>
-
 <template>
   <q-layout view="hHh lpR fFf">
+    <!-- Scroll Progress Bar -->
+    <BlogProgressBar />
+
+    <!-- Header -->
     <HeaderNavbar />
-<<<<<<< HEAD
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-    <FooterSection />
-  </q-layout>
-</template>
-=======
 
+    <!-- Main content -->
     <q-page-container>
       <router-view />
     </q-page-container>
 
+    <!-- Footer -->
     <FooterSection />
   </q-layout>
 </template>
@@ -25,9 +19,5 @@ import HeaderNavbar from 'components/HeaderNavbar.vue'
 <script setup>
 import HeaderNavbar from 'layouts/HeaderNavbar.vue'
 import FooterSection from 'layouts/FooterSection.vue'
+import BlogProgressBar from 'components/BlogProgressBar.vue'
 </script>
-
-<style scoped>
-/* Add any layout-wide styles here */
-</style>
->>>>>>> main
